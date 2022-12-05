0d82ebaa646e1c8fdc6f54ce6c5291bc92c00887
Rafael-Maia2022 <111284115+Rafael-Maia2022@users.noreply.github.com>
2022-12-05 09:32:18 -0300

    Update q4.py

--- q4.py
+++ q4.py
@@ -27,7 +27,7 @@
 # Para obter a nota máxima dessa questão, não deve-se utilizar nenhuma função pronta do Python.
 
 def q4(s):
-    x=q4
+    x=s
     lis=[]
     numer=1
     pala=('Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M')
