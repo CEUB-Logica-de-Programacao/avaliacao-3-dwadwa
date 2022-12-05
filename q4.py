e8566e68b468cb13eaa392d1d7f6de4e09263704
Rafael-Maia2022 <111284115+Rafael-Maia2022@users.noreply.github.com>
2022-12-05 09:43:25 -0300

    Update q4.py

--- q4.py
+++ q4.py
@@ -32,14 +32,14 @@ def q4(s):
     numero=1
     pala=('Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M')
     for y in range(len(x)):
-        lis.append(x[y])
+        lis.append(s[y])
 
     for q in range(len(lis)):
         for w in range(len(pala)):
             if lis[q]==pala[w]:
                 numero=numero+1
 
-    #print(f'há {numero} palavras')
+    
     return numero
 
 
