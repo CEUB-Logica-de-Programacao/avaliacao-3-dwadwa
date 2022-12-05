f65d6385d514e47a1dce93dea44d96a911d0948c
zem-1 <102327065+zem-1@users.noreply.github.com>
2022-12-05 09:19:14 -0300

    Update q1.py

--- q1.py
+++ q1.py
@@ -22,7 +22,12 @@
 # Para obter a nota máxima dessa questão, deve-se utilizar apenas um ``for`` e nenhuma função pronta do Python.
 
 def q1(heights):
-    # Escreva seu código aqui
+    max = 0
+    for h in heights:
+        if h >= 180:
+            heights.remove(h)
+            max +=1
+    print(max)
     return 0
 
 
